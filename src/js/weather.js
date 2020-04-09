49da4b160fcff3b3a64b15ccfee8de5a0bf57b4c
Santiago Rodríguez <santo1996.29@gmail.com>
2020-04-09 15:16:34 -0400

    add temperature values to its component

--- src/js/weather.js
+++ src/js/weather.js
@@ -4,7 +4,12 @@ import 'alertifyjs/build/css/alertify.min.css';
 
 async function getGif(data) {
   const apiKey = 'QSBvewk9ZukYxP00X1KG3XKc2V68uWcG';
-  const response = await fetch(`http://api.giphy.com/v1/gifs/translate?s=${data + 'weather'}&api_key=${apiKey}`, { mode: 'cors' });
+
+  const response = await fetch(
+    `http://api.giphy.com/v1/gifs/translate?s=${data}&api_key=${apiKey}`,
+    { mode: 'cors' }
+  );
+
   const gifData = await response.json();
 
   return gifData.data.images.fixed_width.url;
@@ -12,11 +17,21 @@ async function getGif(data) {
 
 async function curateData(data) {
   const weather = data.weather[0].main;
-  const gif = await getGif(data);
+  const gif = await getGif(weather.toLowerCase());
   const description = data.weather[0].description;
   const icon = data.weather[0].icon;
 
-  return { weather, gif, description, icon };
+  const temperature = {
+    avg: data.main.temp,
+    min: data.main.temp_min,
+    max: data.main.temp_max,
+  };
+
+  return { weather, gif, description, icon, temperature };
+}
+
+function farenheitToCelsius(fahrenheit) {
+  return 5 / 9 * (Number(fahrenheit) - 32);
 }
 
 async function getData(place) {
@@ -26,37 +41,44 @@ async function getData(place) {
   const mainWeather = document.getElementById('weather-main');
   const description = document.getElementById('description');
   const icon = document.createElement('img');
+  const temperatureAvg = document.getElementById('avg');
+  const temperatureMin = document.getElementById('min');
+  const temperatureMax = document.getElementById('max');
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${api_key}`, { mode: 'cors' })
-    .then((response) => {
-      if (response.ok) {
-        alertify.success(`${place} fetched!`);
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${api_key}`,
+    { mode: 'cors' }
+  ).then((response) => {
+    if (response.ok) {
+      alertify.success(`${place} fetched!`);
 
-        response.json().then((data) => {
-          curateData(data).then((curated) => {
-            mainPlace.textContent = place;
-            mainWeather.textContent = curated.weather;
-            icon.src = `http://openweathermap.org/img/w/${curated.icon}.png`;
-            mainWeather.append(icon);
-            description.textContent = curated.description;
-            image.src = curated.gif;
-          });
+      response.json().then((data) => {
+        curateData(data).then((curated) => {
+          mainPlace.textContent = place;
+          mainWeather.textContent = curated.weather;
+          icon.src = `http://openweathermap.org/img/w/${curated.icon}.png`;
+          mainWeather.append(icon);
+          description.textContent = curated.description;
+          temperatureAvg.textContent = curated.temperature.avg + ' ºF';
+          temperatureMin.textContent = curated.temperature.min + ' ºF';
+          temperatureMax.textContent = curated.temperature.max + ' ºF';
+          image.src = curated.gif;
         });
-      } else {
-        alertify.error('Unknown location');
-        mainPlace.textContent = place;
-        mainWeather.textContent = '???';
-        description.textContent = '???';
-        image.src = defaultImage;
-      }
-    })
-    .catch((error) => {
-      alertify.error('Failed fetching the data from the server');
-      mainPlace.textContent = '???';
+      });
+    } else {
+      alertify.error('Unknown location');
+      mainPlace.textContent = place;
       mainWeather.textContent = '???';
       description.textContent = '???';
       image.src = defaultImage;
-    });
+    }
+  }).catch((error) => {
+    alertify.error('Failed fetching the data from the server');
+    mainPlace.textContent = '???';
+    mainWeather.textContent = '???';
+    description.textContent = '???';
+    image.src = defaultImage;
+  });
 }
 
 export { getData };
